16d25e43454b7580bf06fe0566270f456a4b5b70
Cacious Siamunyanga <csiamunyanga@hotmail.com>
2018-04-13 22:02:13 +0800

    creat merge coflict on line 12 for sure

--- app/src/main/java/com/avodev/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/avodev/dinnerdecider/MainActivity.kt
@@ -9,6 +9,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "MacDonals", "Nandos")
+    var caxht = "fellow"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
